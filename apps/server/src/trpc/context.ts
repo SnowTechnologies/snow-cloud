aaf41168aa9acfcade642b43a75f9c8999097591
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-09-06 22:22:12 -0700

    Clean files

--- apps/server/src/trpc/context.ts
+++ apps/server/src/trpc/context.ts
@@ -1,10 +1,10 @@
 import { inferAsyncReturnType } from '@trpc/server';
 import { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
 
+import { prisma } from '@src/db/client';
+
 export function createContext({ req, res }: CreateFastifyContextOptions) {
-    const user = { name: req.headers.username ?? 'anonymous' };
-    
-    return { req, res, user };
+    return { req, res, prisma };
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>;
